75639930d70642e566a901b850e367ca71f71c03
Gonzalo Vazquez <gonzovs@gmail.com>
2024-09-17 19:52:08 +0200

    google account data and sign In

--- app/src/main/java/com/gvs/avisacitas/login/ui/login/googleAccountSignIn/GoogleSignInFragment.java
+++ app/src/main/java/com/gvs/avisacitas/login/ui/login/googleAccountSignIn/GoogleSignInFragment.java
@@ -3,6 +3,7 @@ package com.gvs.avisacitas.login.ui.login.googleAccountSignIn;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,11 +13,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInStatusCodes;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
@@ -44,6 +47,7 @@ public class GoogleSignInFragment extends Fragment {
 		// Configure Google Sign-In
 		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
 				.requestEmail()
+				.requestIdToken("1001618390215-l1c2ok947h8ej60irjvtfdcle8nglur1.apps.googleusercontent.com")
 				.build();
 		mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso);
 
@@ -76,10 +80,47 @@ public class GoogleSignInFragment extends Fragment {
 	private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
 		try {
 			GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-			// Handle successful sign in
-			// You can now navigate to the next screen or handle user data
+
+			if (account == null)
+				return;
+
+			// Extraer información del usuario
+			String accountName = account.getEmail();
+			String displayName = account.getDisplayName();
+			Uri photoUrl = account.getPhotoUrl();
+
+			// Lógica para manejar la cuenta y navegar a otra pantalla o manejar los datos
+			if (accountName == null || displayName == null || photoUrl == null) {
+				Toast.makeText(getContext(), "Account already exists", Toast.LENGTH_SHORT).show();
+				return;
+			}
+
+			// Por ejemplo, puedes pasar estos datos a tu ViewModel
+			mViewModel.saveAccountWithProfileImage(accountName, displayName, photoUrl);
+			Toast.makeText(getContext(), "Account saved: " + accountName, Toast.LENGTH_SHORT).show();
+
 		} catch (ApiException e) {
-			// Handle error
+			// Manejar errores de inicio de sesión
+			Toast.makeText(getContext(), "Sign in failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+			int statusCode = e.getStatusCode();
+			handleSignInError(statusCode);
+		}
+	}
+
+	private void handleSignInError(int statusCode) {
+		switch (statusCode) {
+			case GoogleSignInStatusCodes.SIGN_IN_CANCELLED:
+				Toast.makeText(getContext(), "Sign in cancelled", Toast.LENGTH_SHORT).show();
+				break;
+			case GoogleSignInStatusCodes.SIGN_IN_FAILED:
+				Toast.makeText(getContext(), "Sign in failed", Toast.LENGTH_SHORT).show();
+				break;
+			case GoogleSignInStatusCodes.NETWORK_ERROR:
+				Toast.makeText(getContext(), "Network error", Toast.LENGTH_SHORT).show();
+				break;
+			default:
+				Toast.makeText(getContext(), "Error code: " + statusCode, Toast.LENGTH_SHORT).show();
+				break;
 		}
 	}
 
--- app/src/main/java/com/gvs/avisacitas/login/ui/login/googleAccountSignIn/GoogleSignInViewModel.java
+++ app/src/main/java/com/gvs/avisacitas/login/ui/login/googleAccountSignIn/GoogleSignInViewModel.java
@@ -1,7 +1,175 @@
 package com.gvs.avisacitas.login.ui.login.googleAccountSignIn;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
+import android.telephony.TelephonyManager;
+import android.transition.Transition;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModel;
+
+import com.gvs.avisacitas.Manifest;
+import com.gvs.avisacitas.main.MainActivity;
+import com.gvs.avisacitas.model.accounts.Account;
+import com.gvs.avisacitas.model.sqlite.AvisacitasSQLiteOpenHelper;
+import com.gvs.avisacitas.utils.error.LogHelper;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
 
 public class GoogleSignInViewModel extends ViewModel {
 	// TODO: Implement the ViewModel
+
+    private final AvisacitasSQLiteOpenHelper dbHelper;
+    private static final int STARTING_PK_MCID = 8000;
+    private final Context context;
+
+    public boolean doesAccountExist(String email) {
+        List<Account> existingAccounts = dbHelper.getAllAccounts();
+        for (Account account : existingAccounts) {
+            if (account.getEmail().equalsIgnoreCase(email))
+                return true;
+
+        }
+        return false;
+    }
+
+    public void saveAccountWithProfileImage(String accountName, String displayName, Uri photoUrl) {
+        if (doesAccountExist(accountName)) return;
+
+        Glide.with(getApplication().getApplicationContext())
+                .asBitmap()
+                .load(photoUrl)
+                .into(new CustomTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+
+                        try {
+                            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                            resource.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                            byte[] profileImageBytes = stream.toByteArray();
+                            saveAccount(accountName, displayName, profileImageBytes);
+
+                            Intent intent = new Intent(context, MainActivity.class);
+                            // Añadir la bandera FLAG_ACTIVITY_NEW_TASK.
+                            // Esta bandera indica que la nueva actividad debe iniciarse en una nueva tarea, lo cual es necesario cuando el Context no es una actividad.
+                            // Específicamente, se usa cuando se inicia una actividad desde el contexto de la aplicación.
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            context.startActivity(intent);
+
+                        }catch (Exception ex){
+                            LogHelper.addLogError(ex);
+                        }
+                    }
+                    @Override
+                    public void onLoadCleared(@Nullable Drawable placeholder) {
+                        // Manejar la limpieza de recursos si es necesario
+                    }
+                });
+    }
+
+    private boolean saveAccount(String accountName, String displayName, byte[] profileImageBytes) {
+        String pkMcid = getAvailablePkMcid();
+        String phoneNumber = getPhoneNumber();
+
+        Account account = new Account(
+                displayName != null ? displayName : "",
+                "13133131311313",
+                phoneNumber,
+                pkMcid,
+                accountName,
+                0,
+                "true",
+                0L,
+                0L,
+                0L,
+                "true",
+                "false",
+                "true",
+                "true",
+                "true",
+                "true",
+                "false",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "false",
+                0L
+        );
+
+        account.setProfileImage(profileImageBytes);
+        dbHelper.insertOrUpdateFromObjectList(List.of(account));
+        return true;
+    }
+
+    private String getAvailablePkMcid() {
+        int pkMcid = STARTING_PK_MCID;
+        List<Account> existingAccounts = dbHelper.getAllAccounts();
+        while (true) {
+            boolean isAvailable = true;
+            String pkMcidString = String.valueOf(pkMcid);
+            for (Account account : existingAccounts) {
+                if (account.getPk_mcid().equals(pkMcidString)) {
+                    isAvailable = false;
+                    break;
+                }
+            }
+            if (isAvailable)
+                return pkMcidString;
+
+            pkMcid++;  // Incrementa el pkMcid si ya existe
+        }
+    }
+
+
+    private String getPhoneNumber() {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            return "";
+        }
+
+        SubscriptionManager subscriptionManager = (SubscriptionManager) context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
+
+        if (subscriptionManager != null) {
+            List<SubscriptionInfo> subscriptionInfoList = subscriptionManager.getActiveSubscriptionInfoList();
+
+            if (subscriptionInfoList != null && !subscriptionInfoList.isEmpty()) {
+                // Asumiendo que quieres el número de la primera SIM activa
+                SubscriptionInfo subscriptionInfo = subscriptionInfoList.get(0);
+                String phoneNumber = null;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+                    phoneNumber = subscriptionManager.getPhoneNumber(subscriptionInfo.getSubscriptionId());
+                }
+
+                if (phoneNumber != null && !phoneNumber.isEmpty()) {
+                    return phoneNumber;
+                }
+            }
+        }
+
+
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (telephonyManager != null) {
+            String phoneNumber = telephonyManager.getLine1Number();
+//            String phoneIso = telephonyManager.getNetworkCountryIso();
+//            String phoneSimIso = telephonyManager.getSimCountryIso();
+//            int phoneType = telephonyManager.getPhoneType();
+//            String phoneOperator = telephonyManager.getNetworkOperator();
+//            String phoneOperatorName = telephonyManager.getNetworkOperatorName();
+            if (phoneNumber != null && !phoneNumber.isEmpty())
+                return phoneNumber;
+
+        }
+        // Si llegamos aquí, no se pudo obtener el número de teléfono
+        return "";
+    }
+
 }
